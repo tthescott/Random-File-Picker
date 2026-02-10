18498e35ceb5deda4217c44b06aad2b5a576e2d9
tthescott <sr24unme@gmail.com>
2026-02-10 13:16:59 -0800

    prevent infinite loop, check against self and desktop.ini

--- random.bat
+++ random.bat
@@ -5,10 +5,20 @@ setlocal enabledelayedexpansion
 rem count = number of files in this dir (including this file)
 set /a count=0
 for %%f in ("%~dp0*") do (
-  set /a count+=1
+  rem don't count this file or desktop.ini
+  if /i not "%%~nxf"=="%~nx0" if /i not "%%~nxf"=="desktop.ini" (
+    set /a count+=1
+  )
 )
 
-rem in case it picks this file
+rem in case there's no valid files
+if %count%==0 (
+  echo No valid files found. If you're sure this is a mistake, message me on GitHub
+  pause
+  exit /b 0
+)
+
+rem in case it picks this file or desktop.ini
 :tryAgain
 
 rem get random index
@@ -18,13 +28,19 @@ rem find that index file and open it
 set /a current=0
 for %%f in ("%~dp0*") do (
   if !current!==%rand% (
-    if /i "%%~xf"==".bat" (
+    rem check against this file or desktop.ini
+    if /i "%%~nxf"=="%~nx0" if /i "%%~nxf"=="desktop.ini" (
       goto :tryAgain
     )
     start "" "%%f"
     exit /b 0
   )
-  set /a current+=1
+
+  rem only ++ current if it's on a valid file
+  rem ensures all files are reachable since only valid files were counted
+  if /i not "%%~nxf"=="%~nx0" if /i not "%%~nxf"=="desktop.ini" (
+    set /a current+=1
+  )
 )
 
 rem in case it doesn't find a matching file index
